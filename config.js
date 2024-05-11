b3700f810e790f42522ea4bc05cfe79556a83fd6
Watson FOURPENCE <166027751+Tinashex@users.noreply.github.com>
2024-05-11 10:05:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
@@ -7,14 +6,19 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = ['263781330745', 'Excel', true]
+global.owner = [
+  ['', 'watson-XD', true], 
+  ['263781330745', 'Watson-XD', true], 
+  ['', 'tinashe-XD', true],
+  ['263781330745', 'Watson-XD', true] ]
+
  //Number of owners
 
-//global.pairingNumber = "263781330745" //put your bot number here
+//global.pairingNumber = "" //put your bot number here
 
-global.mods = ['263781330745'] 
-global.prems = ['263781330745']
-global.allowed = ['263781330745']
+global.mods = ['2341', '2348021779696', '263781330745','233533763772'] 
+global.prems = ['2347045035241','233208179431','233533763772']
+global.allowed = ['2348021779696', '2347045035241','233533763772']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -42,33 +46,33 @@ global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://api.neoxr.my.id': `${keysneoxr}`,        
   'https://violetics.pw': 'beta',
   'https://zenzapis.xyz': `${keysxxx}`, 
   'https://api-fgmods.ddns.net': 'fg-dylux'
 }
 
 // Sticker WM
-global.botname = 'Watson-xd'
+global.botname = 'WATSON-𝛸𝐷'
 global.premium = 'true'
-global.packname = 'watson-BOTS_TEAM' 
-global.author = '@EXCEL' 
-global.menuvid = 'https://i.imgur.com/p7sdeTT.mp4'
-global.igfg = '▢✓ Follow My channel_ https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m\n' 
-global.dygp = 'https://chat.whatsapp.com/DNq1MpbdndX5ujuIKlOYaq'
+global.packname = 'watson-XD' 
+global.author = '263781330745' 
+global.menuvid = 'https://telegra.ph/file/ecde07e06bfd1ad56e1fd.png'
+global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaNdKNxK5cDKRztKXV3g\n' 
+global.dygp='▢✓ Follow My channel\nhttps://chat.whatsapp.com/Hbb23H4Ny4RIc3I83S9PEv\n'
 global.fgsc = 'https://github.com/Xcelsama/STAR-MD-V2' 
-global.fgyt = 'https://https://chat.whatsapp.com/DNq1MpbdndX5ujuIKlOYaq
-global.fgpyp = 'https://whatsapp.com/channel/0029Va9wmuz8F2pGIURwmo0m'
+global.fgyt = 'https://chat.whatsapp.com/DNq1MpbdndX5ujuIKlOYaq'
+global.fgpyp = 'https://whatsapp.com/channel/0029VaNdKNxK5cDKRztKXV3g'
 global.fglog = 'STAR.jpg' 
 global.thumb = fs.readFileSync('./STAR.jpg')
 
 
-global.wait = '*🕣 _watson-xd IS LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
-global.rwait = '⌛'
-global.dmoji = '🤭'
-global.done = '✔️'
-global.error = '❌' 
-global.xmoji = '🔥' 
+global.wait = '*🕣 Watson-XD -IS LOADING..._*\n*▰▰▰▱▱▱▱▱🕗*'
+global.rwait = '🔜'
+global.dmoji = '⚡'
+global.done = '☑️'
+global.error = '❎' 
+global.xmoji = '⤵️' 
 
 global.multiplier = 69 
 global.maxwarn = '3' // máxima advertencias
